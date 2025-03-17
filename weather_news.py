3f864de9f826869969b09a1cf8c34927a2f6fbe0
Alex62-Creator <cham62ss@gmail.com>
2025-03-17 13:34:28 +0300

    news

--- weather_news.py
+++ weather_news.py
@@ -8,26 +8,38 @@ app = Flask(__name__)
 #формируем путь и методы GET и POST
 @app.route('/', methods=['GET', 'POST'])
 #создаем функцию с переменной weather, где мы будем сохранять погоду
-def weather():
+def weather_news():
    weather = None
+   news = None
    # формируем условия для проверки метода
    if request.method == 'POST':
        # этот определенный город мы будем брать для запроса API
        city = request.form['city']
        #прописываем переменную, куда будет сохраняться результат и функцию weather с указанием города, который берем из формы
        weather = get_weather(city)
+       # прописываем переменную, куда будет сохраняться результат запроса новостей
+       news = get_news()
        #передаем информацию о погоде в index.html
-   return render_template("weather_news.html", weather=weather)
+   return render_template("weather_news.html", weather=weather, news=news)
 
 #в функции прописываем город, который мы будем вводить в форме
 def get_weather(city):
-   api_key = "945fdp30aac0b74f3de52069536499d8"
+   api_key = "your api key"
    #адрес, по которомы мы будем отправлять запрос
    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
    #для получения результата нам понадобится модуль requests
    response = requests.get(url)
    #прописываем формат возврата результата
    return response.json()
+
+def get_news():
+   api_key = "your api key"
+   # адрес, по которомы мы будем отправлять запрос
+   url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
+   # для получения результата нам понадобится модуль requests
+   response = requests.get(url)
+   # прописываем формат возврата результата
+   return response.json().get('articles', [])
 
 #прописываем запуск
 if __name__ == '__main__':
